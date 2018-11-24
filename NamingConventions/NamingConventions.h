556abd0c57694c8d2e3092070077734d01e252a1
Mani Moayedi <acidmanic.moayedi@gmail.com>
2018-11-24 13:16:29 +0330

    Change UIKit to foundation

--- NamingConventions/NamingConventions.h
+++ NamingConventions/NamingConventions.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Mani Moayedi. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for NamingConventions.
 FOUNDATION_EXPORT double NamingConventionsVersionNumber;
